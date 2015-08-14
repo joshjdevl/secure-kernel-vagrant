5467892f7219e1969ade0b51769162130cb95bd8
josh <joshjdevl@gmail.com>
2015-08-14 10:26:23 +0200

    mkdir bin

--- sel4.sh
+++ sel4.sh
@@ -17,6 +17,7 @@ sudo apt-fast -qq install -y git phablet-tools
 sudo git config --global user.email "<email>"
 
 #http://sel4.systems/Download/
+mkdir -p $HOME/bin/repo
 curl --silent https://storage.googleapis.com/git-repo-downloads/repo > $HOME/bin/repo
 
 chmod a+x $HOME/bin/repo
