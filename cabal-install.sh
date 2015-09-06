7563f64bd251494728c82a9fa4c59afd42d82c37
josh <joshjdevl@gmail.com>
2015-09-06 18:28:38 +0200

    wget continue

--- cabal-install.sh
+++ cabal-install.sh
@@ -20,7 +20,7 @@ sudo -K
 # get distr  
 mkdir -p $HOME/Downloads
 cd $HOME/Downloads
-wget -q "https://www.haskell.org/ghc/dist/$GHC_VERSION/$GHC_DIST_FILENAME"  
+wget -c -q "https://www.haskell.org/ghc/dist/$GHC_VERSION/$GHC_DIST_FILENAME"  
 tar xfj $GHC_DIST_FILENAME  
 cd ghc-$GHC_VERSION  
 
